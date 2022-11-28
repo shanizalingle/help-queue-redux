d47a2668c44643216f57d17ba84b40856fd5ac00
josephjacksonw <josephjacksonw@gmail.com>
2022-11-28 12:14:42 -0500

    third test good fail
    Co-authored-by: shaniza lingle <shanizalingle@users.noreply.github.com>
    Co-authored-by: Ted Gold <tgold83@users.noreply.github.com>
    Co-authored-by: Skylan Lew <doublespoiler@users.noreply.github.com>

--- src/_tests_/reducers/ticket-list-reducer.test.js
+++ src/_tests_/reducers/ticket-list-reducer.test.js
@@ -10,10 +10,24 @@ describe('ticketListReducer', () => {
     id: 1
   };
 
+  const currentState = {
+    1: {
+      names: 'Ryan & Aimen',
+      location: '4b',
+      issue: 'Redux action is not working correctly.',
+      id: 1 
+    }, 2: {
+      names: 'Jasmine and Justine',
+      location: '2a',
+      issue: 'Reducer has side effects.',
+      id: 2 
+    }
+  };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(ticketListReducer({}, { type: null })).toEqual({});
   });
-  
+
   test('Should successfully add new ticket data to mainTicketList', () => {
     const { names, location, issue, id } = ticketData;
     action = {
@@ -30,6 +44,21 @@ describe('ticketListReducer', () => {
         location: location,
         issue: issue,
         id: id
+      }
+    });
+  });
+
+  test('Should successfully delete a ticket', () => {
+    action = {
+      type: 'DELETE_TICKET',
+      id: 1
+    };
+    expect(ticketListReducer(currentState, action)).toEqual({
+      2: {
+        names: 'Jasmine and Justine',
+        location: '2a',
+        issue: 'Reducer has side effects.',
+        id: 2 
       }
     });
   });
